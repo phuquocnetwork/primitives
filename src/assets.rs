65e8bf38a879f1c79ffc9c64a7dacee197e9a332
Thien Tran <fcduythien@gmail.com>
2021-10-01 20:01:49 +0700

    Hello primitives

---
+++ src/assets.rs
@@ -0,0 +1,34 @@
+// This file is part of Phuquocdog.
+
+// Copyright (C) 2020-2021 Phuquocdog oü.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use codec::{Decode, Encode};
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::{RuntimeDebug, H160};
+
+/// Enumerated asset on chain
+#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum AssetId {
+    /// PQD the native currency of the chain
+    PQD,
+    /// Generic enumerated assed
+    /// Range 0 - 0x00000000FFFFFFFF (2^32)-1 is reserved for protected tokens
+    /// the values under 1000 are used for ISO 4217 Numeric Curency codes
+    Asset(u64),
+}
